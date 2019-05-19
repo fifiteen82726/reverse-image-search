3138a783ad05eab7d920d9e30fa89df6e7eedaa8
fifiten82726 <fifiteen82726@gmail.com>
2019-05-19 00:08:44 -0400

    add CBIR folder

--- lib/img_manager.rb
+++ lib/img_manager.rb
@@ -1,13 +1,12 @@
 require 'rmagick'
 
-class Crop
-  def self.hihi
-    p 'hihi'
+class ImgManager
+  def self.crop
     path = Rails.root.join('app', 'assets', 'images', 'paris', 'louvre')
-    path += 'paris_louvre_000008.jpg'
+    path += 'paris_louvre_000022.jpg'
     img = Magick::Image.read(path)[0]
 
-    chopped = img.crop(0, 0, 107, 139)
+    chopped = img.crop(311, 270, 369, 226)
     timestamp = Time.now.to_i.to_s
     name = "python/img/#{Time.now.to_i.to_s}.jpg"
     chopped.write(name)
--- python/feature-extraction.py
+++ python/feature-extraction.py
@@ -1,7 +1,3 @@
-# import numpy as np
-# import pandas as pd
-# import cv2
-# import matplotlib.pyplot as plt
 import os
 import sys
 
@@ -34,16 +30,58 @@ import sys
 
 
 from image_match.goldberg import ImageSignature
-gis = ImageSignature()
 
-if len(sys.argv) > 1:
-  path = 'python/img/' + sys.argv[1] + '.jpg'
-else:
-  path = 'new.jpg'
 
-a = gis.generate_signature(path)
+def generate_sign():
+  gis = ImageSignature()
 
-print a
-# b = gis.generate_signature('https://pixabay.com/static/uploads/photo/2012/11/28/08/56/mona-lisa-67506_960_720.jpg')
+  if len(sys.argv) > 1:
+    path = 'python/img/' + sys.argv[1] + '.jpg'
+  else:
+    path = 'new.jpg'
+
+  a = gis.generate_signature(path)
+
+  print a
+  # b = gis.generate_signature('https://pixabay.com/static/uploads/photo/2012/11/28/08/56/mona-lisa-67506_960_720.jpg')
 # gis.normalized_distance(a, b)
 
+
+
+# Compare two image
+gis = ImageSignature()
+a = '../app/assets/images/paris/louvre/paris_louvre_000142.jpg'
+b = '../app/assets/images/paris/louvre/paris_louvre_000142.jpg'
+
+b = 'img/1557352391.jpg'
+
+
+
+a_s = gis.generate_signature(a)
+b_s = gis.generate_signature(b)
+
+print gis.normalized_distance(a_s, b_s)
+
+from elasticsearch import Elasticsearch
+from image_match.elasticsearch_driver import SignatureES
+
+def add_image():
+  es = Elasticsearch()
+  ses = SignatureES(es)
+  ses = SignatureES(es, distance_cutoff=0.3)
+  ses.add_image('../app/assets/images/paris/louvre/paris_louvre_000142.jpg')
+  ses.add_image('../app/assets/images/paris/louvre/paris_louvre_000004.jpg')
+  ses.add_image('../app/assets/images/paris/louvre/paris_louvre_000008.jpg')
+
+
+# # add_image()
+
+# es = Elasticsearch()
+# ses = SignatureES(es, distance_cutoff=1.0)
+# print ses.search_image('../app/assets/images/paris/louvre/paris_louvre_000004.jpg')
+# print ses.search_image('img/1557352391.jpg')
+
+# # ses.add_image('https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa,_by_Leonardo_da_Vinci,_from_C2RMF_retouched.jpg/687px-Mona_Lisa,_by_Leonardo_da_Vinci,_from_C2RMF_retouched.jpg')
+# # ses.add_image('https://pixabay.com/static/uploads/photo/2012/11/28/08/56/mona-lisa-67506_960_720.jpg')
+# # ses.add_image('https://upload.wikimedia.org/wikipedia/commons/e/e0/Caravaggio_-_Cena_in_Emmaus.jpg')
+# # ses.add_image('https://c2.staticflickr.com/8/7158/6814444991_08d82de57e_z.jpg')
