7e2039720508b0aec6156b580a0870a4d4fef6be
fifiten82726 <fifiteen82726@gmail.com>
2019-05-21 03:40:57 -0500

    fix bugs

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -6,10 +6,17 @@ class ImagesController < ApplicationController
   def search
     info = params['info']
     cate = info['img'].split('/')[0]
-    ImgManager.crop(info['img'], info['x'], info['y'], info['width'], info['height'], cate)
-    return
+
+    info = ImgManager.crop(info['img'], info['x'], info['y'], info['width'], info['height'], cate)
+
+    session[:target] = info[:target]
+    session[:urls] = info[:urls]
+
+    redirect_to show_search_path
   end
 
   def show_search
+    @target = session[:target]
+    @urls = session[:urls]
   end
 end
--- lib/img_manager.rb
+++ lib/img_manager.rb
@@ -2,21 +2,22 @@ require 'rmagick'
 
 class ImgManager
   def self.crop(name, x, y, width, height, cate)
-    # path = Rails.root.join('app', 'assets', 'images', name)
-    # img = Magick::Image.read(path)[0]
+    path = Rails.root.join('app', 'assets', 'images', name)
+    img = Magick::Image.read(path)[0]
 
-    # chopped = img.crop(x.to_i, y.to_i, width.to_i, height.to_i)
-    # timestamp = Time.now.to_i.to_s
-    # save_path = Rails.root.join('public', "#{Time.now.to_i.to_s}.jpg")
-    # chopped.write(save_path)
+    chopped = img.crop(x.to_i, y.to_i, width.to_i, height.to_i)
+    timestamp = Time.now.to_i.to_s
+    save_path = Rails.root.join('public', "#{Time.now.to_i.to_s}.jpg")
+    chopped.write(save_path)
 
     # Search file
-    # binding.pry
+    file = save_path.to_s.split('/')[-1]
+    result = `python3 query.py public/"#{file}" "#{cate}"`
 
-    # file = save_path.to_s.split('/')[-1]
-    # `"cd python && python3 query.py ../public/#{file} #{cate}"`
-    # a = `python3 python/query.py #{save_path} cate`
-    sleep(3)
-    return
+    # return search files
+    return {
+        target: file,
+        urls: result.split("\n")[3..12].map{|r| r.split("\t")[0]}.map{|a| a.sub('database/', '')}
+    }
   end
 end
